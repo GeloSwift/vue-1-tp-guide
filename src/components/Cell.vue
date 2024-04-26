5d7519396c4d5823da7cea43ba4c7cf505d6d761
Etudiant <angelo.nichele317@gmail.com>
2024-04-26 11:04:21 +0200

    morpion

---
+++ src/components/Cell.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+    import { Icon } from '@iconify/vue'
+    import { ref } from 'vue'
+
+    const shape = ref('cross')
+
+    function switchShape() {
+        shape.value = (shape.value=='circle' ? 'cross' : 'circle')
+    }
+    
+
+</script>
+
+
+<template>
+    
+    <div @click="switchShape" class="cell">
+        <Icon v-if="shape=='cross'" icon="radix-icons:cross-1" width="10em" />
+        <Icon v-else                icon="radix-icons:circle" width="10em" />
+    </div>
+
+</template>
+
+
+<style>
+
+    .cell {
+        border: dashed 1px pink;
+    }
+
+</style>
---
+++ src/views/CellTestView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+    import Cell from "@/components/Cell.vue"
+    
+</script>
+
+<template>
+    
+    <h1>Page test d'un composant cellule</h1>
+    <Cell></Cell>
+
+</template>
